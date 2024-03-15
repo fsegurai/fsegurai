f231a4e505cae0ee1307eea5988fe7e1714bea9f
fsegurai <fsegurai@ucenfotec.ac.cr>
2024-03-15 11:46:59 -0600

    feat/initial profile

---
+++ index.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+const Mustache = require('mustache');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const puppeteerService = require('./services/puppeteer.service');
+
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+let DATA = {
+  refresh_date: new Date().toLocaleDateString('en-US', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+    timeZone: 'America/Costa_Rica',
+  }),
+};
+
+async function setWeatherInformation() {
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+  )
+    .then(r => r.json())
+    .then(r => {
+      DATA.city_temperature = Math.round(r.main.temp);
+      DATA.city_weather = r.weather[0].description;
+      DATA.city_weather_icon = r.weather[0].icon;
+      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'America/Costa_Rica',
+      });
+      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'America/Costa_Rica',
+      });
+    });
+}
+
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+  });
+}
+
+async function action() {
+  /**
+   * Fetch Weather
+   */
+  await setWeatherInformation();
+
+  /**
+   * Generate README
+   */
+  await generateReadMe();
+
+  /**
+   * Fermeture de la boutique 👋
+   */
+  await puppeteerService.close();
+}
+
+action();
